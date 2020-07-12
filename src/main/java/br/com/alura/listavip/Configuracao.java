912bf0da559c93f19186ee0b42f91472ce4dbb84
FERNANDA SILVA/LGESP CNS - SSP(arrazi.silva@lgepartner.com) <arrazi.silva@lge.com>
2020-07-12 13:05:56 -0300

    Correção Tomezone MySQL

--- src/main/java/br/com/alura/listavip/Configuracao.java
+++ src/main/java/br/com/alura/listavip/Configuracao.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Configuração {
+public class Configuracao {
 	
 	public static void main(String[] args) throws Exception {
-		SpringApplication.run(Configuração.class, args);
+		SpringApplication.run(Configuracao.class, args);
 	}
 
 //	@Bean
